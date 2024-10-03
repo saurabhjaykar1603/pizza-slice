286292f3e41748f0f3695f1e6015fc53039ab506
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-10-03 18:20:14 +0530

    move page.tsx into (home)

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+function Home() {
+  return (
+    <main>
+      <h1>Welcome to pizza shop</h1>
+      <Button variant="default">Click me</Button>
+    </main>
+  );
+}
+
+export default Home;
