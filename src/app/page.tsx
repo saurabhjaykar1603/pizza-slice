2718638d52062df71146ebb083d0a84de7526a8b
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-10-03 17:26:41 +0530

    add shadcn ui theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 function Home() {
   return (
     <main>
       <h1>Welcome to pizza shop</h1>
+      <Button variant="default">Click me</Button>
     </main>
   );
 }
